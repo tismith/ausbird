7d1ce8bcabb1dec0370ed73120b8f0e37d35adb7
Toby Smith <toby@tismith.id.au>
2014-02-24 08:26:42 +1000

    Switch to using an error-handling parseTime

--- Main.hs
+++ Main.hs
@@ -1,11 +1,12 @@
-module Main where 
+module Main (main) where 
 
 import Database.HDBC (SqlValue, quickQuery', fromSql, disconnect)
 import Database.HDBC.Sqlite3 (connectSqlite3)
 import Text.CSV (Record, printCSV)
-import Data.Time (UTCTime)
-import Data.Time.Format(readTime, formatTime)
+import Data.Time (UTCTime, getCurrentTime)
+import Data.Time.Format(parseTime, formatTime)
 import System.Locale(defaultTimeLocale)
+import Data.Maybe(fromMaybe)
 
 {-
  - eBird record format, CSV:
@@ -18,20 +19,21 @@ import System.Locale(defaultTimeLocale)
  - CREATE TABLE tblListBirds (id INTEGER PRIMARY KEY, birdID INTEGER, birdComName VARCHAR(35), birdTaxNum INTEGER, birdLocation VARCHAR(100), dateOfSighting VARCHAR(20), birdComments VARCHAR(450), gpsLat REAL, gpsLong REAL);
 -}
 
--- FIXME: use parseTime to catch parse errors
-fixDate :: String -> String
-fixDate rawDate = formatTime defaultTimeLocale "%-m/%-d/%Y" rawTime
-    where rawTime = readTime defaultTimeLocale "%Y-%m-%d" rawDate :: UTCTime
+fixDate :: UTCTime -> String -> String
+fixDate defaultTime rawDate = formatTime defaultTimeLocale "%-m/%-d/%Y" rawTime
+    where rawTime = fromMaybe defaultTime $ parseTime defaultTimeLocale "%Y-%m-%d" rawDate
 
-parseRow:: [SqlValue] -> Record
-parseRow (_:_:name:_:location:rawDate:comments:lat:long:[])= [fromSql name,"","","x", fromSql comments, fromSql location, fromSql lat, fromSql long, date,"","","","casual","1","","N","","",""]
-    where date = fixDate $ fromSql rawDate
-parseRow _ = [""]
+-- FIXME use safeFromSql to catch parse errors
+parseRow:: UTCTime -> [SqlValue] -> Record
+parseRow defaultTime (_:_:name:_:location:rawDate:comments:lat:long:[]) = [fromSql name,"","","x", fromSql comments, fromSql location, fromSql lat, fromSql long, date,"","","","casual","1","","N","","",""]
+    where date = fixDate defaultTime $ fromSql rawDate
+parseRow _ _ = [""]
 
 main = do
+    today <- getCurrentTime
     conn <- connectSqlite3 "AUSBDBList.sql"
     rows <- quickQuery' conn "SELECT * FROM tblListBirds" []
-    writeFile "eBird.csv" $ printCSV $ map parseRow rows
+    writeFile "eBird.csv" $ printCSV $ map (parseRow today) rows
     disconnect conn
 
 
